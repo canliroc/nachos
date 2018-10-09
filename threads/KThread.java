3ffe9227309abca863dd1654a5c40edebc92f7b5
canliroc <canliroc@pace.edu>
2018-10-09 08:43:48 +0100

    Assignment-1

--- threads/KThread.java
+++ threads/KThread.java
@@ -388,7 +388,9 @@ public class KThread {
 	
 	public void run() {
 	    for (int i=0; i<5; i++) {
-		System.out.println("*** thread " + which + " looped "
+		//System.out.println("*** thread " + which + " looped "
+		//		   + i + " times");
+		System.out.println("*** Hello, sweet thread " + which + " looped "
 				   + i + " times");
 		currentThread.yield();
 	    }
